34fe85fc34be085d619df63d1709dbe2cc0408ec
VikasSastiya <vikassastiya06@gmail.com>
2024-09-17 16:15:44 +0530

    project

--- Week-7/Week-7-1/week-7-1/src/App.jsx
+++ Week-7/Week-7-1/week-7-1/src/App.jsx
@@ -1,56 +1,48 @@
-import { useState, useContext } from "react";
+import { useContext, useState} from "react"
 import { CountContext } from "./context";
 
-// reducer/useReducer
+
 function App() {
   const [count, setCount] = useState(0);
+  
+  // wrap anyone that wants to use the teleported value inside a provider
   return (
-    // wrap anyone that wants to use the teleported value inside a provider
     <div>
-      <CountContext.Provider value={count}>
+      <CountContext.Provider value={{
+        count, setCount
+      }}>
         <Count setCount={setCount} />
       </CountContext.Provider>
     </div>
-  );
+  )
 }
 
-function Count({ setCount }) {
-  return (
-    <div>
-      <CountRenderer />
-      <Buttons setCount={setCount} />
-    </div>
-  );
+function Count({setCount}) {
+  return <div>
+    <CountRenderer />
+    <Buttons setCount={setCount}/>
+  </div>
 }
 
 function CountRenderer() {
-  // Use the count value from CountContext
-  const count = useContext(CountContext);
-  return <div>{count}</div>;
+  const {count} = useContext(CountContext);
+
+  return <div>
+    {count}
+  </div>
 }
 
-function Buttons({ setCount }) {
-  // Use the count value from CountContext
-  const count = useContext(CountContext);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Increase
-      </button>
+function Buttons({setCount}) {
+  const count= useContext(CountContext);
+  return <div>
+    <button onClick={() => {
+      setCount(count + 1)
+    }}>Increase</button>
 
-      <button
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        Decrease
-      </button>
-    </div>
-  );
+    <button onClick={() => {
+      setCount(count - 1)
+    }}>Decrease</button>
+  </div>
 }
 
-export default App;
+export default App
--- Week-7/Week-7-1/week-7-1/src/components/Dashboard.jsx
+++ Week-7/Week-7-1/week-7-1/src/components/Dashboard.jsx
@@ -1,26 +1,8 @@
-import { children } from "react"
+
 
 export default function Dashboard() {
+
     return <div>
-        Dashboard Page
-      <Boldify>
-        hi there
-      </Boldify>
-      <Boldify>
-        hi there
-      </Boldify>
-
-      <Boldify>
-        hi there
-      </Boldify>
-      <Boldify>
-        hi there
-      </Boldify>
-    </div>   
-}
-
-function Boldify({children}) {
-    return <b>
-        {children}
-    </b>
+        Dashboard page
+    </div>
 }
